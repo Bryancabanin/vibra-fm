5195f65e712323ebde1eb230f1f0dcf0e25d932c
Bryan <bryancabanin@gmail.com>
2026-06-11 21:51:15 -0400

    feat: style Navbar with logo, active nav links, and user dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,56 @@
 import { NavLink } from 'react-router';
+import { useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const { user, logout } = useAuth();
+
   return (
-    <nav>
-      <NavLink to='/discover'>Discover</NavLink>
-      <NavLink to='/history'>History</NavLink>
+    <nav className={styles.navbar}>
+      <div>
+        <span className={styles.logo}>Vibra FM</span>
+      </div>
+
+      <div className={styles.navRight}>
+        {/* right container */}
+        <div className={styles.navLinksContainer}>
+          <NavLink
+            to='/discover'
+            className={({ isActive }) =>
+              isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+            }
+          >
+            Discover
+          </NavLink>
+          <NavLink
+            to='/history'
+            className={({ isActive }) =>
+              isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+            }
+          >
+            History
+          </NavLink>
+        </div>
+
+        <div className={styles.userMenu}>
+          <button
+            className={styles.userButton}
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
+            {user?.spotify_id}
+          </button>
+
+          {isDropdownOpen && (
+            <div className={styles.dropdownContainer}>
+              <button className={styles.dropdownItem} onClick={logout}>
+                Log out
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
     </nav>
   );
 };
