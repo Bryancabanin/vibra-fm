0799ee0f0d4bad61de42dd002c1367b56d8f8170
Bryan <bryancabanin@gmail.com>
2026-06-11 21:51:27 -0400

    feat: add content padding and max-width to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,13 @@
 import type { ReactNode } from 'react';
 import Navbar from './Navbar';
+import styles from './Layout.module.css';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <Navbar />
       {/* Render the page that we pass in */}
-      {children}
+      <div className={styles.content}>{children}</div>
     </div>
   );
 };
