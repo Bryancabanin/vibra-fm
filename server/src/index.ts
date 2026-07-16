a99f16e808c6da6040b1c6100b86f673be3cf3eb
Bryan <bryancabanin@gmail.com>
2026-07-15 20:36:53 -0400

    fix: bind server to 0.0.0.0 instead of 127.0.0.1

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,6 @@ if (!process.env.PORT) {
   throw new Error('Invalid or missing PORT in .env');
 }
 // need application to listen to PORT
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`Server is running on 127.0.0.1:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
